14bc67a4a961576c06aa6f31bfe184551db4bfa0
theacebutler <theacebutler@gmail.com>
2026-01-08 14:42:27 -0500

    moved hidden link in to it own component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { defaultLinks, type Link } from "./defaultLinks"
 import LinkList from "./LinkList";
 import Input from "./Input"
+import AddHidden from "./AddHidden";
 import "./index.css";
 
 
@@ -18,7 +19,6 @@ export function App() {
 
   const [linkList, setLinkList] = useState<Link[]>(links())
 
-  const [hidden, setHidden] = useState<boolean>(true)
 
 
 
@@ -29,23 +29,11 @@ export function App() {
       <Input setLinkList={setLinkList} linkList={linkList} />
       {/* link list */}
       <LinkList linkList={linkList} />
-      <div className="flex flex-col align-middle">
-        <button
-          onClick={() => {
-            setHidden(!hidden)
-          }}
-        >Show hidden links</button>
-        {
-          !hidden && <div className="items-center">
-            <li>
-              <a href="https://google.com">This is a hidden link</a>
-            </li>
-          </div>
-        }
         <button>
           <a href="https://google.com">Link in a button</a>
         </button>
       </div>
+      <AddHidden />
     </main >
   );
 }
