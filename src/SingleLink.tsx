c9fbc09e7dc3394492a5bd018c4b9074f753d62d
theacebutler <theacebutler@gmail.com>
2026-01-08 21:05:11 -0500

    added error handleing for firefox

--- src/SingleLink.tsx
+++ src/SingleLink.tsx
@@ -2,15 +2,19 @@ import type { Link } from "./defaultLinks"
 
 export default function SingleLink({ link }: { link: Link }) {
 
-  const handleDrage = (e: React.DragEvent<HTMLLIElement>) => {
+  function handleDrage(e: React.DragEvent<HTMLLIElement>) {
     e.currentTarget.style.position = "absolute"
     e.currentTarget.style.left = `${e.clientX}px`
     e.currentTarget.style.top = `${e.clientY}px`
+    if (e.currentTarget.style.top === "0px" && e.currentTarget.style.left === "0px") {
+      window.alert("As of now, dragging works only in Chrome and not in FireFox or FireFox based browsers (Yes, Zen. that includes you)")
+      e.currentTarget.style.position = ""
+      return
+    }
   }
   return <li
     draggable
-    onDragEnd={handleDrage}
-  // onDragEnd={handleDrageEnd}
+    onDrag={handleDrage}
   >
     <a
       className=""
